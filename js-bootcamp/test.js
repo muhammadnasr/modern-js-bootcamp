39961f6c13cf8e0c0650640e50779fceabaf7b3f
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-07-13 19:26:27 +0300

    init commit

---
+++ js-bootcamp/test.js
@@ -0,0 +1,23 @@
+let min = 5
+let max = 12
+let randomNum = Math.random() * (max - min) + min
+
+
+class person  {
+          #name
+          methpd(){
+
+          }
+}
+age=10
+debugger
+console.log(age)
+
+console.log(['a',{name:'ehat'}].join(''))
+
+var fo = {
+a(){
+
+},
+b:1;
+}
