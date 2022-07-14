f6d6dadbc9f6022eda50f829e8a13bd346e2a836
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-07-14 18:48:15 +0300

    adds sorting and makes todos a private variable

--- js-bootcamp/todo-app/src/scripts/todos-service.js
+++ js-bootcamp/todo-app/src/scripts/todos-service.js
@@ -1,31 +1,74 @@
 import { v4 as uuidv4 } from 'uuid'
+import moment from 'moment'
 
 export class TodosService {
 
     static #todos = []
 
-    static filters = {
-        searchText: '', 
-        hideCompleted: false
+    static #filters = {
+        searchText: '',
+        hideCompleted: false,
+        sortBy: 'byEdited'
     }
-    
+
     static getFilteredTodos = () => {
-        return todos.filter((todo) => {
-            const searchTextMatch = todo.text.toLowerCase().includes(this.filters.searchText.toLowerCase())
-            const hideCompletedMatch = !this.filters.hideCompleted || !todo.completed
-    
+        let sortedTodos = this.#getSortedTodos()
+        return sortedTodos.filter((todo) => {
+            const searchTextMatch = todo.text.toLowerCase().includes(this.#filters.searchText.toLowerCase())
+            const hideCompletedMatch = !this.#filters.hideCompleted || !todo.completed
+
             return searchTextMatch && hideCompletedMatch
-        })    
+        })
+    }
+
+    static #getSortedTodos = () => {
+        switch (this.#filters.sortBy) {
+            case 'byEdited':
+                return this.#todos.sort((a, b) => {
+                    if (a.updatedAt > b.updatedAt) {
+                        return -1
+                    } else if (a.updatedAt < b.updatedAt) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+            case 'byCreated':
+                return this.#todos.sort((a, b) => {
+                    if (a.createdAt > b.createdAt) {
+                        return -1
+                    } else if (a.createdAt < b.createdAt) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+            case 'alphabetical':
+                return this.#todos.sort((a, b) => {
+                    if (a.text.toLowerCase() < b.text.toLowerCase()) {
+                        return -1
+                    } else if (a.text.toLowerCase() > b.text.toLowerCase()) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+            default:
+                return this.#todos
+        }
     }
 
     static getIncompletedTodos = () => this.getFilteredTodos().filter((todo) => !todo.completed)
-    
-    static setFilters = ({ searchText, hideCompleted }) => {
+
+    static setFilters = ({ searchText, hideCompleted, sortBy }) => {
         if (typeof searchText === 'string') {
-            this.filters.searchText = searchText
+            this.#filters.searchText = searchText
         }
         if (typeof hideCompleted === 'boolean') {
-           this.filters.hideCompleted = hideCompleted
+            this.#filters.hideCompleted = hideCompleted
+        }
+        if (typeof sortBy === 'string') {
+            this.#filters.sortBy = sortBy
         }
     }
 
@@ -34,41 +77,46 @@ export class TodosService {
         const todosJSON = localStorage.getItem('todos')
 
         try {
-            todos = todosJSON ? JSON.parse(todosJSON) : []
+            this.#todos = todosJSON ? JSON.parse(todosJSON) : []
         } catch (e) {
-            todos = []
+            this.#todos = []
         }
     }
 
     // Save todos to localStorage
     static saveTodos = () => {
-        localStorage.setItem('todos', JSON.stringify(todos))
+        localStorage.setItem('todos', JSON.stringify(this.#todos))
     }
 
     static createTodo = (text) => {
-        todos.push({
+        const timestamp = moment().valueOf()
+
+        this.#todos.push({
             id: uuidv4(),
             text,
-            completed: false
+            completed: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
         })
         TodosService.saveTodos()
     }
 
     static removeTodo = (id) => {
-        const todoIndex = todos.findIndex((todo) => todo.id === id)
+        const todoIndex = this.#todos.findIndex((todo) => todo.id === id)
 
         if (todoIndex > -1) {
-            todos.splice(todoIndex, 1)
+            this.#todos.splice(todoIndex, 1)
             TodosService.saveTodos()
         }
     }
 
     // Toggle the completed value for a given todo
     static toggleTodo = (id) => {
-        const todo = todos.find((todo) => todo.id === id)
+        const todo = this.#todos.find((todo) => todo.id === id)
 
         if (todo) {
             todo.completed = !todo.completed
+            todo.updatedAt = moment().valueOf()
             TodosService.saveTodos()
         }
     }
