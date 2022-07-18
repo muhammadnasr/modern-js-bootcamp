9da745381075d92ff9fb87358c2c1ab801692f20
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-07-18 18:03:37 +0300

    refactor boilerplate to use parcel and delete unused files

--- js-bootcamp/test.js
+++
@@ -1,9 +0,0 @@
-let score = 5
-let getNewScoreMessage = function () {
-    //shadowing
-    let score = 2
-    return `Shadowing score: ${score}`
-}
-let result = getNewScoreMessage()
-console.log(`${result}, Original score: ${score}`)
-
